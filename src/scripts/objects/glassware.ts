e13aa0164b10741bd6699e7cc266e64cef2830ca
Anna Wu <annapoliswu@gmail.com>
2020-07-15 19:31:59 -0400

    some buttons, figuring out masking

--- src/scripts/objects/glassware.ts
+++ src/scripts/objects/glassware.ts
@@ -2,30 +2,34 @@ import { GameObjects } from "phaser";
 
 export default class Glassware extends Phaser.GameObjects.Sprite {
     glasstype: string;
-    sprite: GameObjects.Sprite;
     target: number;
     max: number;
     fill: number = 0;
-
+    fillImage: GameObjects.Sprite;
+    mask;
+    maskShape; 
+    graphics;
+    x;
+    y;
     constructor(scene: Phaser.Scene, x: number, y: number, typeinput: string) {
         super(scene, x, y, typeinput); 
-        //player = new Player(this, 0, 0, 'image');
-        //this.add.existing(player);
+        this.setTintFill(0xCCC); //gradient [topLeft] [, topRight] [, bottomLeft] [, bottomRight]
+    
         this.glasstype = typeinput;
         switch(this.glasstype){
             case "beaker":
                 this.target = 200;
                 this.max = 800;
-                scene.add.image(x,y,'beakerFill');
-                //this.setTintFill(0xfff, 0xCCC); // [topLeft] [, topRight] [, bottomLeft] [, bottomRight]
                 break;
-        
         }
         scene.add.existing(this);
     }
 
-    update(){
-
+        //player = new Player(this, 0, 0, 'image');
+        //this.add.existing(player);
+    addWater(){
+        this.fill += this.max/20;
+        this.maskShape.y -= this.max/20
     }
 
     
--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -1,5 +1,6 @@
 import ExampleObject from '../objects/exampleObject';
 import Glassware from "../objects/glassware";
+import { GameObjects } from 'phaser';
 
 /*
 const WIDTH
@@ -7,17 +8,71 @@ const HEIGHT
 */
 export default class MainScene extends Phaser.Scene {
   private exampleObject: ExampleObject;
-  beaker : Glassware;
+  glassware : Glassware;
+  glasswareGroup;
 
+  addButton: GameObjects.Text;
+  minusButton;
+  graphics;
+  fillImage;
+  mask;
   constructor() {
     super({ key: 'MainScene' });
   }
   
   create() {
+    let x = 500;
+    let y = 300;
+    this.glassware = new Glassware(this, x, y, "beaker").setDepth(99);
     
-    this.beaker = new Glassware(this, 500, 300, "beaker");
+    this.graphics = this.add.graphics();
+    //this.graphics.fillStyle(0xff00ff, 1);
+    this.fillImage = new GameObjects.Sprite(this, x,y,this.glassware.glasstype+'Fill');
+    let w = this.fillImage.width*2;
+    let maskShape = this.graphics.fillRoundedRect(x-w/2, y- this.fillImage.height/2, w , this.fillImage.height, { tl: 0, tr: 0, bl: w/2, br: w/2 });
+    
+    this.mask = this.fillImage.createGeometryMask(this.graphics);
+    this.mask.invertAlpha = true;
+    this.fillImage.setMask(this.mask);
+    //this.fillImage.clearMask(); //mask off of lineart for some reason
+    maskShape.y-=100;
+
+    this.add.existing(this.fillImage);
+
+    //let graphics2 = this.add.graphics();
+    this.addButton = this.add.text(700, 200, 'ADD WATER', { backgroundColor: '#000', padding:20})
+      .on('pointerover', () => {this.buttonHover(this.addButton)})
+      .on('pointerout', () => {this.buttonRest(this.addButton)})
+      .on('pointerdown', () => {this.buttonDown(this.addButton)})
+      .on('pointerup', () => {
+        //addwater
+        this.buttonHover(this.addButton);
+      });
+    this.addButton.setInteractive();
+
+    this.minusButton = this.add.text(700, 300, 'SUBTRACT WATER', { backgroundColor: '#000', padding:20})
+    .on('pointerover', () => {this.buttonHover(this.minusButton)})
+    .on('pointerout', () => {this.buttonRest(this.minusButton)})
+    .on('pointerdown', () => {this.buttonDown(this.minusButton)})
+    .on('pointerup', () => {
+      //addwater
+      this.buttonHover(this.minusButton);
+    });
+    this.minusButton.setInteractive();
   }
 
+
   update() {
+  }
+
+  
+  buttonHover(button){
+    button.setStyle({backgroundColor:'#00CCCC'});
+  }
+  buttonRest(button){
+    button.setStyle({backgroundColor:'#000'});
+  }
+  buttonDown(button){
+    button.setStyle({backgroundColor:'#00AAAA'});
   }
 }
