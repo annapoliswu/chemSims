df7dcd71d44fab152b5801a2de949ca2f82db61d
Anna Wu <annapoliswu@gmail.com>
2020-07-16 22:49:59 -0400

    Updates

---
+++ dev/precache-manifest.0462ec7f21337f55455282468a6410a5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab9c1175e9f88ba278def4fb8f20e3d",
+    "url": "/educational-phaser-game-template/devassets/beaker150ml.png"
+  },
+  {
+    "revision": "6cec8b3b89256df0325301089a36bb1a",
+    "url": "/educational-phaser-game-template/devassets/beaker200ml.png"
+  },
+  {
+    "revision": "53fe89a7e2e405a962ee5d71bd305fd5",
+    "url": "/educational-phaser-game-template/devassets/beaker800ml.png"
+  },
+  {
+    "revision": "24b76318b0d3521fbac94c8b6ddde4ac",
+    "url": "/educational-phaser-game-template/devassets/beaker800ml_fill.png"
+  },
+  {
+    "revision": "ee55a1346782b58db641428b60c29db7",
+    "url": "/educational-phaser-game-template/devassets/beaker800ml_fill2.png"
+  },
+  {
+    "revision": "6ef042ac6cf9c63e3e3271c69e4a7682",
+    "url": "/educational-phaser-game-template/devassets/citations.md"
+  },
+  {
+    "revision": "5190202528c0a6e30c7962efc0efd1ee",
+    "url": "/educational-phaser-game-template/devassets/erlenmeyerflask.png"
+  },
+  {
+    "revision": "17c873e852eab2ffb5c40d47fae8835e",
+    "url": "/educational-phaser-game-template/devassets/graduatedcylinder.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-phaser-game-template/devassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/educational-phaser-game-template/devassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/educational-phaser-game-template/devassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/educational-phaser-game-template/devassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/educational-phaser-game-template/devassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-phaser-game-template/devfavicon.ico"
+  },
+  {
+    "revision": "c57b3d7326521ff16d36",
+    "url": "/educational-phaser-game-template/devgen.main.5fb59e8614a30e712f30.bundle.js"
+  },
+  {
+    "revision": "ae41a841fddcd7a3cc7b",
+    "url": "/educational-phaser-game-template/devgen.vendors.e8da5af998faa49d7e73.bundle.js"
+  },
+  {
+    "revision": "34e9d34c0f8f67d3dab744d9764d98e6",
+    "url": "/educational-phaser-game-template/devindex.html"
+  },
+  {
+    "revision": "003187cab0ecf7a20e343e2ba6db4a34",
+    "url": "/educational-phaser-game-template/devmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/educational-phaser-game-template/devsw.js"
+  }
+]);
