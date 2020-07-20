f7aa8be94c7bd25ca07c6e166b4839369f5088ba
Anna Wu <annapoliswu@gmail.com>
2020-07-20 00:51:51 -0400

    trying to get deploy working pt 2

--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -7,7 +7,7 @@ export default class PreloadScene extends Phaser.Scene {
     this.load.image('beaker','assets/beaker800ml.png');
     this.load.image('beakerFill','assets/beaker800ml_fill.png');
     this.load.image('scale','assets/scale.png' );
-    this.load.html('form', 'src/scripts/objects/form.html');
+    this.load.html('form', 'assets/form.html');
   }
 
   create() {
