d613a69664b1a5b9f21f6e8046c524eaedfcb300
Anna Wu <annapoliswu@gmail.com>
2020-07-19 22:27:12 -0400

    input figured out, formatting form

--- src/scripts/objects/notes.ts
+++ src/scripts/objects/notes.ts
@@ -1,23 +1,29 @@
 export default class Notes {
 
+    initialWeight: number;
+    finalWeight: number
+
+
     constructor(scene: Phaser.Scene, x: number, y: number, w: number, h: number) {
         scene.add.rectangle(x, y, w, h, 0x000 );
         //scene.add.text(x,y,"*interactive notes go here");
-
-        let f = scene.add.dom(x, y).createFromCache('form');
+        
         let header = scene.add.text(800,100,"");
 
-        f.addListener('click');
-        f.on('click', function (event) {
-            if (event.target.name === 'playButton'){
+        /*note: must wrap returned doc element with HTMLInputElement for inputs; no value property on HTMLElement */
+        let form = scene.add.dom(x, y).createFromCache('form');
+
+        form.addListener('click');
+        form.on('click', (event) => {
+            if (event.target.name === 'initialSubmit'){
                 
-                header.text = "playHit"
+                header.text = "buttonHit"
                 
-                let element = document.getElementById("text"); //gets html part
-                let inputText = document.getElementById("name");
+                let element = document.getElementById("initialWeightLabel"); //gets html part
+                this.initialWeight = parseFloat((<HTMLInputElement>document.getElementById("initialWeight")).value);
                 if(element){
-                    if(inputText && inputText.innerHTML){ //cannot use inner on input types; nothing between html tags
-                        element.innerText = "aghhh"; //how to extract input value???
+                    if(this.initialWeight){
+                        element.innerText = "Initial Weight: " + this.initialWeight;
                     }else{
                         element.innerText = "null input";
                     }
