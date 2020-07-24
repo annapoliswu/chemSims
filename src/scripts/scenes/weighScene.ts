b6dedd5113e009ad8b23125c5413a957b2671056
Anna Wu <annapoliswu@gmail.com>
2020-07-23 21:12:41 -0400

    color changes, calculation fix

--- src/scripts/scenes/weighScene.ts
+++ src/scripts/scenes/weighScene.ts
@@ -29,6 +29,10 @@ export default class WeighScene extends BaseScene {
 
     temp: number;
     density: number;
+    mass: number; 
+    volume: number;
+    targetMass: number;
+    targetVolume: number;
 
     
 
@@ -45,13 +49,25 @@ export default class WeighScene extends BaseScene {
         super.create();
         this.add.rectangle(this.WIDTH / 2, this.HEIGHT - 100, this.WIDTH, 200, 0x999999).setDepth(-99);
         this.chemScale = this.add.image(this.WIDTH/3.5, this.HEIGHT-180,'scale');
+
+        this.makeNotes();
+        this.createGlassware(this.WIDTH/3.5, 240, this.waterAmountStart);
+        this.glassware.on('pointerover', () => {
+            this.glassware.alpha = .2;
+        }).on('pointerout', ()=> {
+            this.glassware.alpha = 1;
+        }).on('pointerdown', ()=> {
+            this.glassware.setTintFill(0xFF00FF);
+        }).on('pointerup', ()=> {
+            this.glassware.setTintFill(0xCCC); 
+            this.pastWaterScene = true;
+            this.scene.start('WaterScene', {glasstype: this.glasstype});
+        });
+        
         this.temp = 70;
         this.density = 0.99802;
-
-        //this.notes = new Notes(this, 3*this.WIDTH/4, this.HEIGHT/2, this.WIDTH/2.5, this.HEIGHT);
-        this.makeNotes();
-
-        this.createGlassware(this.WIDTH/3.5, 240, this.waterAmountStart);
+        this.targetMass = this.glassware.target;
+        this.targetVolume = this.glassware.target/this.density;
 
         this.scaleText = this.add.text(210, 575, this.glassware.weight + this.waterAmountStart + ' g', {
             fontSize: '60px',
@@ -63,7 +79,7 @@ export default class WeighScene extends BaseScene {
         this.warning = this.add.text(820,100,"",{
             fontFamily: 'Arial',
             fontSize: '24px',
-            color: '#FF0000',
+            color: '#FF00FF',
             wordWrap: {
                 width: 600
             }
@@ -77,18 +93,6 @@ export default class WeighScene extends BaseScene {
             wordWrap: {
                 width: 600
             }
-        });
-        
-        this.glassware.on('pointerover', () => {
-            this.glassware.alpha = .2;
-        }).on('pointerout', ()=> {
-            this.glassware.alpha = 1;
-        }).on('pointerdown', ()=> {
-            this.glassware.setTintFill(0xFF0000);
-        }).on('pointerup', ()=> {
-            this.glassware.setTintFill(0xCCC); 
-            this.pastWaterScene = true;
-            this.scene.start('WaterScene', {glasstype: this.glasstype});
         });
 
     }
@@ -119,14 +123,14 @@ export default class WeighScene extends BaseScene {
                     this.iWeight = parseFloat(this.iWeightElement.value);
                     if(this.iWeightLabel){
                         if(this.iWeight){
-                            this.iWeightLabel.textContent = "Initial Weight: " + this.iWeight;
+                            this.iWeightLabel.textContent = "Initial Weight: " + this.iWeight + " g";
                                 if(this.iWeight === this.glassware.weight){
                                     //can hide visibility like this 
                                     this.iWeightElement.style.display = "none";
                                     this.iSubmitElement.style.display = "none";
 
-                                    this.warning.text = "<== CLICK GLASS TO ADD WATER";
-                                    this.glassware.setTintFill(0xFF0000);
+                                    this.warning.text = "<- Click glass to add water";
+                                    this.glassware.setTintFill(0xFF00FF);
                                     this.glassware.setInteractive();
                                 }else{
                                     this.warning.setText("Check the scale again");
@@ -139,7 +143,7 @@ export default class WeighScene extends BaseScene {
             });
         } else if(this.pastWaterScene){
             //scene resets this stuff if not stated? create run multiple times? not starting new scene, same because values for iWeight stay the same??
-            this.iWeightLabel.textContent = "Initial Weight: " + this.iWeight;
+            this.iWeightLabel.textContent = "Initial Weight: " + this.iWeight + " g";
             this.iWeightElement.style.display = "none";
             this.iSubmitElement.style.display = "none";
 
@@ -149,20 +153,19 @@ export default class WeighScene extends BaseScene {
                     this.fWeight = parseFloat(this.fWeightElement.value);
                     if(this.fWeightLabel){
                         if(this.fWeight){
-                            this.fWeightLabel.textContent = "Final Weight: " + this.fWeight;
+                            this.fWeightLabel.textContent = "Final Weight: " + this.fWeight + " g";
                                 if(this.fWeight === this.glassware.weight+this.glassware.waterAmount){
-                                    //can hide visibility like this 
                                     this.fWeightElement.style.display = "none";
                                     this.fSubmitElement.style.display = "none";
                                     this.warning.setText("");
 
-                                    //replace with variables: density, temp, mass
+                                    this.calcVars();
                                     this.calcText.setText("DENSITY = MASS / VOLUME" +
                                     "\nVOLUME = MASS / DENSITY" +
                                     "\n\nWater's density varies slightly with its temperature" +
                                     "\nIf the water's temperature is " + this.temp + " °F, it's density is " + this.density + " g/ml"+
-                                    "\n\nVOLUME = " + (this.glassware.weight+this.glassware.waterAmount) + " g / " + this.density + " g/ml"+
-                                    "\nVOLUME = " + (this.glassware.weight+this.glassware.waterAmount)/this.density + " ml");
+                                    "\n\nVOLUME = " + this.mass + " g / " + this.density + " g/ml"+
+                                    "\nVOLUME = " + this.volume + " ml");
                                 }else{
                                     this.warning.setText("Check the scale again");
                                 }
@@ -176,6 +179,10 @@ export default class WeighScene extends BaseScene {
 
     }
 
+    calcVars(){
+        this.mass = this.fWeight - this.iWeight;
+        this.volume = this.mass/this.density;
+    }
 
     update() {
     }
