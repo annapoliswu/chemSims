7ced3df57dbd409e2cd63975c148829ec0600e9c
Anna Wu <annapoliswu@gmail.com>
2020-09-20 04:01:03 -0400

    Updates

---
+++ dev/precache-manifest.b497943e796da3a4618d5d615ee9064d.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab9c1175e9f88ba278def4fb8f20e3d",
+    "url": "/chemSims/devassets/beaker150ml.png"
+  },
+  {
+    "revision": "6cec8b3b89256df0325301089a36bb1a",
+    "url": "/chemSims/devassets/beaker200ml.png"
+  },
+  {
+    "revision": "05bdf36b6ac4a13da36abe9a17428078",
+    "url": "/chemSims/devassets/beaker50ml.png"
+  },
+  {
+    "revision": "fa9c628fdfa3d2f9f95c38c93186ef72",
+    "url": "/chemSims/devassets/beaker50ml_fill.png"
+  },
+  {
+    "revision": "53fe89a7e2e405a962ee5d71bd305fd5",
+    "url": "/chemSims/devassets/beaker800ml.png"
+  },
+  {
+    "revision": "24b76318b0d3521fbac94c8b6ddde4ac",
+    "url": "/chemSims/devassets/beaker800ml_fill.png"
+  },
+  {
+    "revision": "ee55a1346782b58db641428b60c29db7",
+    "url": "/chemSims/devassets/beaker800ml_fill2.png"
+  },
+  {
+    "revision": "ebb65ee92b1547ff04eb6493c76aa6e5",
+    "url": "/chemSims/devassets/citations.md"
+  },
+  {
+    "revision": "5190202528c0a6e30c7962efc0efd1ee",
+    "url": "/chemSims/devassets/erlenmeyerflask.png"
+  },
+  {
+    "revision": "051dd3ea3e16c71bf197fb0f2fb01db6",
+    "url": "/chemSims/devassets/form.html"
+  },
+  {
+    "revision": "17c873e852eab2ffb5c40d47fae8835e",
+    "url": "/chemSims/devassets/graduatedcylinder.png"
+  },
+  {
+    "revision": "458ddedf88e0fc8052c54eeb189fe9d3",
+    "url": "/chemSims/devassets/graduatedcylinder10ml.png"
+  },
+  {
+    "revision": "6ab90e6fb0ac6dad0cee2624a664d503",
+    "url": "/chemSims/devassets/graduatedcylinder10ml_fill.png"
+  },
+  {
+    "revision": "a35cb65aaad77f679cdd2f907a40b04d",
+    "url": "/chemSims/devassets/icons/favicon.ico"
+  },
+  {
+    "revision": "1f3ac3159944a9dab19ce3a91750801e",
+    "url": "/chemSims/devassets/icons/icons-16.png"
+  },
+  {
+    "revision": "53a1f1dc6278173316cbdcc1f1535af1",
+    "url": "/chemSims/devassets/icons/icons-192.png"
+  },
+  {
+    "revision": "38583fbe36b38398bf47d11dbfb990ab",
+    "url": "/chemSims/devassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/chemSims/devassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "2b8870d85709343e62cf0d0081754324",
+    "url": "/chemSims/devassets/inputLine.html"
+  },
+  {
+    "revision": "c411a95d1ed424b4b36f93f47f31599f",
+    "url": "/chemSims/devassets/orig_scale.png"
+  },
+  {
+    "revision": "0dad6c9af2f8e41a9ffbd2f63bd3e9f1",
+    "url": "/chemSims/devassets/scale.png"
+  },
+  {
+    "revision": "7d8e7e924baf9be9256197e04023fd51",
+    "url": "/chemSims/devassets/volumetricflask10ml.png"
+  },
+  {
+    "revision": "11cb9045aeda495a37fac9969b2ee966",
+    "url": "/chemSims/devassets/volumetricflask10ml_fill.png"
+  },
+  {
+    "revision": "fed9f0e05ea3b224723e82cf468ac872",
+    "url": "/chemSims/devassets/watertable.png"
+  },
+  {
+    "revision": "a35cb65aaad77f679cdd2f907a40b04d",
+    "url": "/chemSims/devfavicon.ico"
+  },
+  {
+    "revision": "cd478dd2544b4fe8cbb8",
+    "url": "/chemSims/devgen.main.598a69761f07adc6096b.bundle.js"
+  },
+  {
+    "revision": "357472365fc941d5bfb0",
+    "url": "/chemSims/devgen.vendors.6eff77b6c7acbbf363af.bundle.js"
+  },
+  {
+    "revision": "8717abae66d713fa0aa7c047be0cfac5",
+    "url": "/chemSims/devindex.html"
+  },
+  {
+    "revision": "b7dd28bff9a72cd3f058018070ac2ef0",
+    "url": "/chemSims/devmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/chemSims/devsw.js"
+  }
+]);
---
+++ dev/sw.js
@@ -0,0 +1,11 @@
+importScripts("/chemSims/devprecache-manifest.b497943e796da3a4618d5d615ee9064d.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
